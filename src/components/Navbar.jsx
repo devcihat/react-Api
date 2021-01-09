184aa386bdaf9161d7a668bd0e0b9cc9082ec6f0
devcihat <cihattsungu@outlook.com>
2021-01-09 17:47:02 +0300

    1 file modified, 1 added, 2 deleted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MovieGif from "./movie.gif";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-light bg-light ">
       <div className="container-fluid  ">
         <Link className="navbar-brand" to="/">
-          <i className="fas fa-video fa-lg mx-2" />
+          <iframe
+            src="https://giphy.com/embed/1gdoeJmc3piKTeORzG"
+            width="30"
+            height="30"
+          />
           <b>Movie Storage</b>
         </Link>
         <div style={{ cursor: "pointer" }}>
