7b448da835f15b1e322b57409a3f330cf35c1879
devcihat <cihattsungu@outlook.com>
2021-01-11 18:51:09 +0300

    1 file modified

--- src/components/error/ERROR404.jsx
+++ src/components/error/ERROR404.jsx
@@ -18,7 +18,7 @@ function ERROR404(props) {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-xs-12 col-lg-12">
+        <div className="col-xs-12 col-md-12 col-lg-12">
           <div className="text-center " ref={container}>
             <Link to="/" className="text-white">
               <i className="fas fa-directions fa-lg mx-1" /> <b>Home Page</b>
